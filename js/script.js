2a0d61bc415c5693bc8484d0b5a17a97fafa5515
Kyle Peeler <kylepeeler@yahoo.com>
2024-12-18 20:18:04 -0800

    clarification on input IDs. added input classes for customization

--- js/script.js
+++ js/script.js
@@ -2,18 +2,19 @@ $(document).ready(()=>{
     let apikey = "placeholderkey";
     $(".search-form").submit(()=>{
         event.preventDefault();
-        let searchTerm = $("#searchinput").val();
+        let searchTerm = $("#gifinput").val();
+        let gifcount = $("#numberinput").val();
         
         $.ajax({
             url:
-            `https://api.giphy.com/v1/gifs/search?api_key=ZeZBqIUsPLa69oSOp4pDTgvbAiw5S29F&q=${searchTerm}&limit=15&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
+            `https://api.giphy.com/v1/gifs/search?api_key=ZeZBqIUsPLa69oSOp4pDTgvbAiw5S29F&q=${searchTerm}&limit=${gifcount}&offset=0&rating=g&lang=en&bundle=messaging_non_clips`
         })
         .done((response) => {
             let gifData = response.data;
             $.each(gifData, (i, e)=>{
                 let gifImg= e.images.fixed_width.webp
                 $(".gifcontainer").append(`
-                    <img class = "col" src="${gifImg}" 
+                    <img src="${gifImg}" 
                     alt="gif of ${searchTerm}"/>
                     `)
             })
