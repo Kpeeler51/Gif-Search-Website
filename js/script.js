e039f7be42b08db39c936e622951fae9840e3b4a
Kyle Peeler <kylepeeler@yahoo.com>
2024-12-20 18:50:44 -0800

    comments added to js/script.js to explain code.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
+// Wait until the DOM is fully loaded before running JavaScript
 $(document).ready(()=>{
 
-    let apikey = "placeholderkey";
-
+    // default search function disabled. Instead term searched will
+    // become searchTerm and amount of gifs requested will become gifCount.
     $(".search-form").submit(()=>{
         $(".gifcontainer").empty();
         event.preventDefault();
         let searchTerm = $("#gifinput").val();
-        let gifcount = $("#numberinput").val();
+        let gifCount = $("#numberinput").val();
         
+        // .ajax requests searchTerm and gifCount from the Giphy API.
+        //.done() function is used to handle the response.
+        //.each() function loops through the response data and appends each GIF to the HTML.
+        // The image is also given an alt attribute containing the GIF's alt text from giphy API.
         $.ajax({
             url:
-            `https://api.giphy.com/v1/gifs/search?api_key=ZeZBqIUsPLa69oSOp4pDTgvbAiw5S29F&q=${searchTerm}&limit=${gifcount}&lang=en&bundle=messaging_non_clips&rating=r`
+            `https://api.giphy.com/v1/gifs/search?api_key=ZeZBqIUsPLa69oSOp4pDTgvbAiw5S29F&q=${searchTerm}&limit=${gifCount}&lang=en&bundle=messaging_non_clips&rating=r`
         })
         .done((response) => {
             let gifData = response.data;
@@ -25,6 +30,9 @@ $(document).ready(()=>{
 
         })
     })
+
+    // When user clicks on a GIF, it's copied to clipboard.
+    // The user is alerted when text is copied to the clipboard.
     $(document).on('click', '.image-copy', function () {
         const imageUrl = $(this).attr('src');
 
